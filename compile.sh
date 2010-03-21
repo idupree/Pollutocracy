078cdf32d8561a3fd18293cc46c61b1ad2d56716
Isaac Dupree <antispam@idupree.com>
2010-03-21 17:19:42 +0000

    neater build script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
-gcc -c -I/usr/include/ -O3 -pipe -time -Wall foreignPollution.c && \
- ghc -c -fffi -I/usr/include/SDL/ -Wall -fglasgow-exts --make Main.hs "$@" && \
- echo 'Linking...' && \
- ghc -package GLUT -lGL -lGLU -lglut -lSDL -lm *.o
+set -e
+gcc -c -I/usr/include/ -O3 -pipe -time -Wall foreignPollution.c
+ghc -c -fffi -I/usr/include/SDL/ -Wall -fglasgow-exts --make Main.hs "$@"
+echo 'Linking...'
+ghc -package GLUT -lGL -lGLU -lglut -lSDL -lm *.o
 
