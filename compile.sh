59436480effb2ae66ba6acf25bb9ef1a3510436c
Isaac Dupree <antispam@idupree.com>
2010-03-21 21:00:12 +0000

    fix for compiling with profiling

--- compile.sh
+++ compile.sh
@@ -10,5 +10,5 @@ hsc2hs -I/usr/include/SDL/ *.hsc
 gcc -c -I/usr/include/ -O3 -pipe -time -Wall foreignPollution.c
 ghc -c -I/usr/include/SDL/ -Wall --make Main.hs "$@"
 echo 'Linking...'
-ghc -o game -package GLUT -lGL -lGLU -lglut -lSDL -lm *.o
+ghc -o game -package GLUT -lGL -lGLU -lglut -lSDL -lm *.o "$@"
 
