15d2d64276d63286d263d80a59b52c8e30d8f8e0
Isaac Dupree <antispam@idupree.com>
2010-03-21 19:09:27 +0000

    put build in its own directory

--- compile.sh
+++ compile.sh
@@ -1,5 +1,11 @@
 #!/bin/sh 
 set -e
+
+rm -rf build || true
+mkdir build
+cp *.hs *.hsc *.c *.h build
+cd build
+
 hsc2hs -I/usr/include/SDL/ *.hsc
 gcc -c -I/usr/include/ -O3 -pipe -time -Wall foreignPollution.c
 ghc -c -fffi -I/usr/include/SDL/ -Wall -fglasgow-exts --make Main.hs "$@"
