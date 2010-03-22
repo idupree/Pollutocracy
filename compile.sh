ebb454ab3312aca2a8e4b37867fc67e25b65fa00
Isaac Dupree <antispam@idupree.com>
2010-03-22 00:31:25 +0000

    tweak compile.sh

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ cp *.hs *.hsc *.c *.h build
 cd build
 
 #hsc2hs -I/usr/include/SDL/ *.hsc
-gcc -c -I/usr/include/ -O3 -pipe -time -Wall foreignPollution.c
+gcc -c -I/usr/include/ -O3 -Wall foreignPollution.c #-O0 -ggdb #-pipe -time
 ghc -c -Wall --make Main.hs "$@"
 echo 'Linking...'
 #no -lSDL
