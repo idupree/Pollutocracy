9dc2963cc5ed198e0300055c0306b20da1a7ab0f
Isaac Dupree <antispam@idupree.com>
2011-01-29 06:01:02 +0000

    add a bit of user-documentation

--- compile.sh
+++ compile.sh
@@ -1,6 +1,21 @@
 #!/bin/sh 
 set -e
 
+# To build this game, you need
+# * a Haskell development environment, namely, GHC
+# * the C GLUT & OpenGL libraries
+# * the Haskell 'clock' and 'GLUT' libraries, which can be installed
+#     by 'cabal install clock' and 'cabal install GLUT' if you have
+#     a recent Haskell Platform.
+# * Also you need a bit of POSIX, because the 'clock' library depends on it.
+#     I know Linux works, I'm sure Mac OS X does, and maybe MinGW
+#     or even plain Windows does...I haven't tried it.
+# * I haven't tried it outside of the GCC / Linux world, so it might just
+#     not work elsewhere, though it "should".
+#
+# Then run ./compile.sh and hope it works!
+
+
 #instead always deleting/redoing the build directory
 #rm -rf build || true; mkdir build; cp ...,
 #rsync -u doesn't update files whose modification-times haven't changed
