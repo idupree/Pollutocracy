aabfbf6e3f59f3c8c4295d4c3526941aabe26d84
Isaac Dupree <antispam@idupree.com>
2011-01-29 05:22:17 +0000

    neaten compile.sh comments a bit

--- compile.sh
+++ compile.sh
@@ -1,16 +1,15 @@
 #!/bin/sh 
 set -e
 
-#instead of this, let's try letting it not recompile sometimes
-#rm -rf build || true
-#mkdir build
-#cp
+#instead always deleting/redoing the build directory
+#rm -rf build || true; mkdir build; cp ...,
 #rsync -u doesn't update files whose modification-times haven't changed
 rsync -u *.hs *.hsc *.c *.h build
 #although, if you want the warnings again, or different minor compile options,
 #hmm...
 cd build
 
+#we don't use SDL anymore
 #hsc2hs -I/usr/include/SDL/ *.hsc
 gcc -c -I/usr/include/ -O3 -Wall foreignPollution.c #-O0 -ggdb #-pipe -time
 ghc -c -Wall --make Main.hs "$@"
