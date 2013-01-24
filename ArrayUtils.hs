a8986b83f161e09ce07856eda8962f6c6e28778d
Isaac Dupree <antispam@idupree.com>
2013-01-24 01:02:13 +0000

    clean up ArrayUtils

--- ArrayUtils.hs
+++ ArrayUtils.hs
@@ -8,8 +8,4 @@ amapWithIxs f arr = array (bounds arr) (map (\i -> (i, f i (arr ! i))) (indices 
 
 arraySize :: (IArray a e) => a (Int,Int) e -> (Int,Int)
 arraySize arr = case bounds arr of ((x1,y1),(x2,y2)) -> (x2 - x1 + 1, y2 - y1 + 1)
---runST (do
---		mArr <- newArray_
---		
---	)
 
